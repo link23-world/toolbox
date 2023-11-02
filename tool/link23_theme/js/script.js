e5367822185fdeaf2609c2e498bea70337eab7fb
Raphael Sonabend <raphael.sonabend@genomicsengland.co.uk>
2023-11-02 11:53:16 +0000

    add theme

---
+++ tool/link23_theme/js/script.js
@@ -0,0 +1,39 @@
+// ---------
+// Variables
+// ---------
+// Current version number
+const version = "0.1.0";
+// Full name of the tool
+const fullName = "<Full name>";
+// Short name of the tool for page titles
+const shortName = "<Short name>";
+// GitHub repository
+const repo = "<repo-name>";
+// Zenodo DOI when you have it
+const zenodo = "<doi_1>/zenodo.<doi_2>";
+
+// ----------------
+// Populate fields
+// ----------------
+$(document).ready(function () {
+  $("#header").load("link23_theme/shared/header.html");
+  $("#footer").load("link23_theme/shared/footer.html");
+});
+
+$(document).ajaxStop(function () {
+  const today = new Date();
+  const year = today.getFullYear();
+
+  $(".fullname").text(fullName);
+  $(".navbar-brand").text(shortName);
+  $(".navbar-brand").attr("href", `/tool/index.html`);
+  $(".btn-version").text("v" + version);
+  $(".btn-version").attr("href", `https://github.com/link23-world/${repo}/blob/main/NEWS.md`);
+  $(".repo").attr("href", `https://github.com/link23-world/${repo}`);
+  $(".news").attr("href", `https://github.com/link23-world/${repo}/blob/main/NEWS.md`);
+  $(".contributors").attr("href", `https://github.com/link23-world/${repo}#contributors`);
+  $(".license").attr("href", `https://github.com/link23-world/${repo}/blob/main/LICENSE.md`);
+  $(".issue").attr("href", `https://github.com/link23-world/${repo}/issues`);
+  $(".discussion").attr("href", `https://github.com/link23-world/${repo}/discussions`);
+  $(".citation").text(`The link23 Community. (${year}). ${fullName} (Version ${version}) [Computer software]. https://doi.org/${zenodo}.`);
+});
